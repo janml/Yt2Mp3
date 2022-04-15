722e5ea704fdd15078501ba0df986513e1726af6
janml <janmichaelloehlein@gmail.com>
2022-04-15 14:48:01 +0200

    Add event listeners for video search form

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,12 @@
 
 
+
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded and parsed.")
+  const videoUrlField = document.getElementById("video-url-field")
+  const searchButton = document.getElementById("search-button")
+
+  searchButton.addEventListener("click", () => {
+    const videoUrl = videoUrlField.value
+    console.log(`Requested to search for video with url: ${videoUrl}`)
+  })
 })
