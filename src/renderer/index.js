0e41969aacac72a01bd2160be32398f9ccfa0ec8
janml <janmichaelloehlein@gmail.com>
2022-04-15 15:35:19 +0200

    Toggling the visibility of the video search result

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -3,9 +3,12 @@
 window.addEventListener("DOMContentLoaded", () => {
   const videoUrlField = document.getElementById("video-url-field")
   const searchButton = document.getElementById("search-button")
+  const videoSearchResultCard = document.getElementById("video-search-result-card")
 
   searchButton.addEventListener("click", () => {
     const videoUrl = videoUrlField.value
     console.log(`Requested to search for video with url: ${videoUrl}`)
+
+    videoSearchResultCard.classList.remove("is-invisible")
   })
 })
