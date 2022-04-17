56923075d71e4418c9aeb43fc7a22bf7c6f72e98
janml <janmichaelloehlein@gmail.com>
2022-04-17 15:24:37 +0200

    Remove zombie code

--- src/worker/ipc.js
+++ src/worker/ipc.js
@@ -1,14 +1,8 @@
-const fs = require('fs')
-const ytdl = require("ytdl-core")
 const {ipcMain} = require("electron")
 const {downloadYoutubeVideoAsMp3} = require("./tasks")
 
 
 const initIPC = () => {
-  ipcMain.on("get-video-meta", async (event, videoUrl) => {
-    event.reply("video-meta-resolved", await ytdl.getInfo(videoUrl))
-  }),
-
   ipcMain.on("download-video-as-mp3", async (event, videoUrl) => {
     await downloadYoutubeVideoAsMp3(videoUrl, (progress) => {
       event.sender.send("download-progress", progress)
--- src/worker/preload.js
+++ src/worker/preload.js
@@ -3,16 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld(
   "youtube", {
-
-    getVideoMeta: async (videoUrl) => {
-      ipcRenderer.send("get-video-meta", videoUrl)
-      return await new Promise((resolve, reject) => {
-        ipcRenderer.on("video-meta-resolved", (event, videoMeta) => {
-          resolve(videoMeta)
-        })
-      })
-    },
-    
     downloadVideoAsMp3: async (videoUrl, onProgress) => {
       ipcRenderer.send("download-video-as-mp3", videoUrl)
       ipcRenderer.on("download-progress", (event, progress) => {onProgress(progress)})
