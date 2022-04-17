f3285e66e90838d53caee8a236ae57cf52fff87c
janml <janmichaelloehlein@gmail.com>
2022-04-17 15:21:22 +0200

    Remove clutter from the ui script

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,41 +2,22 @@
 
 window.addEventListener("DOMContentLoaded", async () => {
   const videoUrlField = document.getElementById("video-url-field")
-  const videoSearchButton = document.getElementById("video-search-button")
-  const videoMetaCard = document.getElementById("video-meta-card")
-  const videoTitle = document.getElementById("video-title")
-  const videoThubmnail = document.getElementById("video-thumbnail")
   const downloadStartButton = document.getElementById("download-start-button")
-  const downloadProgressbar = document.getElementById("download-progressbar")
   const downloadProgressDescribtion = document.getElementById("download-progress-describtion")
-
-  let videoUrl = String()
-
-  videoSearchButton.addEventListener("click", async () => {
-    videoUrl = videoUrlField.value
-    const videoMeta = await window.youtube.getVideoMeta(videoUrl)
-    console.log(videoMeta)
-
-    videoTitle.innerText = `${videoMeta.videoDetails.title.substring(0, 20)} ...`
-    videoThubmnail.src = videoMeta.videoDetails.thumbnails[0].url
-    videoMetaCard.classList.remove("is-invisible")
-  })
+  const downloadProgressbar = document.getElementById("download-progressbar")
 
   downloadStartButton.addEventListener("click", async () => {
-    downloadStartButton.classList.add("is-invisible")
-    downloadProgressbar.classList.remove("is-invisible")
-
-    await window.youtube.downloadVideoAsMp3(videoUrl, (progress) => {
+    videoUrl = videoUrlField.value
+    
+    await window.youtube.downloadVideoAsMp3(videoUrl, async (progress) => {
       const percent = ((progress.processed / progress.total) * 100).toFixed(2)
+      const totalMB = (progress.total / 1024 / 1024).toFixed(2)
+      const progressedMB = (progress.processed / 1024 / 1024).toFixed(2)
+      
       downloadProgressbar.value = percent
-      downloadProgressDescribtion.innerText = `Progress: ${percent} % (${(progress.processed / 1024 / 1024).toFixed(2)} / ${(progress.total / 1024 / 1024).toFixed(2)} MB)`
-
-      console.log(`${percent} % downloaded`)
+      downloadProgressDescribtion.innerText = `Progress: ${percent} % (${progressedMB} / ${totalMB} MB)`
     })
 
-    downloadProgressbar.classList.add("is-invisible")
-    downloadStartButton.classList.remove("is-invisible")
     downloadProgressbar.value = 0
-    downloadProgressDescribtion.innerText = "Download finished."
   })
 })
